ea0d7e0825e3a3b03f14afe31c5360f98a700375
Janicke Jacobs <u21674605@tuks.co.za>
2024-06-08 21:05:35 +0200

    Added all the input fields for the confirm details page

--- lib/screens/confirm_details.dart
+++ lib/screens/confirm_details.dart
@@ -1,15 +1,333 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ConfirmDetailsScreen extends StatelessWidget {
+class ConfirmDetailsScreen extends StatefulWidget {
+  @override
+  _ConfirmDetailsScreenState createState() => _ConfirmDetailsScreenState();
+}
+
+class _ConfirmDetailsScreenState extends State<ConfirmDetailsScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _username = '';
+  String _spiceLevel = 'None';
+  List<String> _dietaryRestrictions = [];
+  String _cuisine = 'Option 1';
+  File? _profileImage;
+
+  final List<String> _spiceLevels = ['None', 'Mild', 'Medium', 'Hot', 'Very Hot'];
+  final List<String> _dietaryOptions = ['Option 1', 'Option 2', 'Option 3'];
+  final List<String> _cuisineOptions = ['Option 1', 'Option 2'];
+
+  Future<void> _pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _profileImage = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  void _handleSignup() {
+    if (_formKey.currentState!.validate()) {
+      // Navigate to home page or handle signup
+      Navigator.pushReplacementNamed(context, '/home');
+    }
+  }
+
+  void _addDietaryRestriction(String restriction) {
+    setState(() {
+      _dietaryRestrictions.add(restriction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Confirm Details'),
-      ),
-      body: Center(
-        child: Text('This is the Confirm Details screen'),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/Darkmode.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'logo.png',
+                          height: 80,
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Culinary Companion',
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 32),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: _pickImage,
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: _profileImage != null
+                            ? FileImage(_profileImage!)
+                            : AssetImage('assets/default_profile.png') as ImageProvider,
+                        child: _profileImage == null
+                            ? Icon(Icons.add, size: 50, color: Colors.white)
+                            : null,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      width: 365,
+                      height: 70,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: 'Username:',
+                          labelStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                          hintText: 'username',
+                          hintStyle: const TextStyle(color: Color(0xFF778579)),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFA9B8AC),
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFDC945F),
+                              width: 2.0,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 12.0,
+                            horizontal: 12.0,
+                          ),
+                          filled: false,
+                          fillColor: Colors.transparent,
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) => _username = value,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      width: 365,
+                      height: 70,
+                      child: DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: 'Preferred spice level:',
+                          labelStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFA9B8AC),
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFDC945F),
+                              width: 2.0,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 12.0,
+                            horizontal: 12.0,
+                          ),
+                          filled: false,
+                          fillColor: Colors.transparent,
+                        ),
+                        value: _spiceLevel,
+                        items: _spiceLevels.map((String level) {
+                          return DropdownMenuItem<String>(
+                            value: level,
+                            child: Text(level),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _spiceLevel = newValue!;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            width: 365,
+                            height: 70,
+                            child: DropdownButtonFormField<String>(
+                              decoration: InputDecoration(
+                                floatingLabelBehavior: FloatingLabelBehavior.always,
+                                labelText: 'Dietary restrictions:',
+                                labelStyle: const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  borderSide: const BorderSide(
+                                    color: Color(0xFFA9B8AC),
+                                    width: 2.0,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  borderSide: const BorderSide(
+                                    color: Color(0xFFDC945F),
+                                    width: 2.0,
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 12.0,
+                                  horizontal: 12.0,
+                                ),
+                                filled: false,
+                                fillColor: Colors.transparent,
+                              ),
+                              items: _dietaryOptions.map((String option) {
+                                return DropdownMenuItem<String>(
+                                  value: option,
+                                  child: Text(option),
+                                );
+                              }).toList(),
+                              onChanged: (newValue) {
+                                _addDietaryRestriction(newValue!);
+                              },
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.add, color: Colors.white),
+                          onPressed: () {
+                            // Add logic to add more dietary restrictions
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      width: 365,
+                      height: 70,
+                      child: DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: 'Preferred cuisine:',
+                          labelStyle: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFA9B8AC),
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                              color: Color(0xFFDC945F),
+                              width: 2.0,
+                            ),
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 12.0,
+                            horizontal: 12.0,
+                          ),
+                          filled: false,
+                          fillColor: Colors.transparent,
+                        ),
+                        value: _cuisine,
+                        items: _cuisineOptions.map((String cuisine) {
+                          return DropdownMenuItem<String>(
+                            value: cuisine,
+                            child: Text(cuisine),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _cuisine = newValue!;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    Container(
+                      width: 365,
+                      height: 46,
+                      child: ElevatedButton(
+                        onPressed: _handleSignup,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFDC945F),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          side: const BorderSide(
+                            color: Colors.transparent,
+                            width: 2.0,
+                          ),
+                        ),
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      child: const Text('Already have an account? Login'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
