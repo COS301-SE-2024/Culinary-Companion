36ef32ff006e43b7d157705b627b290efa0b67d1
Janicke Jacobs <u21674605@tuks.co.za>
2024-06-14 23:25:59 +0200

    Added image background to the Appliances page
    Please have a look to see if this looks fine, otherwhise I can remove it

--- lib/screens/appliances_screen.dart
+++ lib/screens/appliances_screen.dart
@@ -111,100 +111,113 @@ class _AppliancesPageState extends State<AppliancesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-        child: Row(
-          children: <Widget>[
-            // Pantry List Column
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(
-                  0, // left padding
-                  20.0, // top padding
-                  0.0, // right padding
-                  0.0, // bottom padding
-                ), // Adjust the top padding as needed
-                child: Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.start, // Left-align children
-                  children: <Widget>[
-                    const Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text(
-                        'Appliances',
-                        style: TextStyle(
-                          fontSize: 24.0, // Set the font size for h2 equivalent
-                          fontWeight: FontWeight.bold, // Make the text bold
-                        ),
-                        textAlign:
-                            TextAlign.left, // Ensure text is left-aligned
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: appliances.length,
-                        itemBuilder: (context, index) {
-                          final appliance = appliances[index];
-                          return Card(
-                            color: index.isEven ? Color(0xFF1D2C1F) : Color(0xFF344E46),
-                            margin: EdgeInsets.symmetric(vertical: 8.0),
-                            child: ListTile(
-                              contentPadding: EdgeInsets.symmetric(
-                                horizontal: 16.0,
-                                vertical: 8.0,
-                              ),
-                              leading: Icon(Icons.kitchen, color: Colors.white),
-                              title: Text(
-                                appliance,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              trailing: IconButton(
-                                icon: Icon(Icons.delete_outline, color: Colors.white),
-                                onPressed: () {
-                                  _removeAppliance(appliance);
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton(
-                        onPressed: _showAddApplianceDialog,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(
-                              0xFFDC945F), // Button background color
-                          foregroundColor: Colors.white, // Text color
-                          fixedSize: const Size(
-                              48.0, 48.0), // Ensure the button is square
-                          shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.circular(16), // Rounded corners
-                          ),
-                          padding:
-                              const EdgeInsets.all(0), // Remove default padding
-                        ),
-                        child: const Text(
-                          '+',
-                          style: TextStyle(
-                            fontSize: 35,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+      body: Stack(
+        children: <Widget>[
+          Container(  
+            decoration: BoxDecoration(
+              image: DecorationImage(  
+                image: AssetImage('background.png'),
+                fit: BoxFit.cover,
               ),
             ),
-          ],
-        ),
+          ),
+        //Foreground content
+        Padding(  
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: Row(
+            children: <Widget>[
+              // Pantry List Column
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(
+                    0, // left padding
+                    20.0, // top padding
+                    0.0, // right padding
+                    0.0, // bottom padding
+                  ), // Adjust the top padding as needed
+                  child: Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start, // Left-align children
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Appliances',
+                          style: TextStyle(
+                            fontSize: 24.0, // Set the font size for h2 equivalent
+                            fontWeight: FontWeight.bold, // Make the text bold
+                          ),
+                          textAlign:
+                              TextAlign.left, // Ensure text is left-aligned
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: appliances.length,
+                          itemBuilder: (context, index) {
+                            final appliance = appliances[index];
+                            return Card(
+                              color: index.isEven ? Color(0xFF1D2C1F) : Color(0xFF344E46),
+                              margin: EdgeInsets.symmetric(vertical: 8.0),
+                              child: ListTile(
+                                contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 16.0,
+                                  vertical: 8.0,
+                                ),
+                                leading: Icon(Icons.kitchen, color: Colors.white),
+                                title: Text(
+                                  appliance,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                trailing: IconButton(
+                                  icon: Icon(Icons.delete_outline, color: Colors.white),
+                                  onPressed: () {
+                                    _removeAppliance(appliance);
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: _showAddApplianceDialog,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(
+                                0xFFDC945F), // Button background color
+                            foregroundColor: Colors.white, // Text color
+                            fixedSize: const Size(
+                                48.0, 48.0), // Ensure the button is square
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(16), // Rounded corners
+                            ),
+                            padding:
+                                const EdgeInsets.all(0), // Remove default padding
+                          ),
+                          child: const Text(
+                            '+',
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
