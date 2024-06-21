fc12d65accfcde56c1b3b6117505c1f9645a506d
u21463230 <u21463230@tuks.co.za>
2024-06-21 17:21:18 +0200

    Correcting Camel Case

--- lib/widgets/help_home.dart
+++ lib/widgets/help_home.dart
@@ -100,23 +100,23 @@ class HelpMenu extends StatelessWidget {
     );
   }
 
-  Widget _buildSectionTitle(String title, Color FontColor) {
+  Widget _buildSectionTitle(String title, Color fontColor) {
     return Text(
       title,
       style: TextStyle(
-          fontSize: 18, fontWeight: FontWeight.bold, color: FontColor),
+          fontSize: 18, fontWeight: FontWeight.bold, color: fontColor),
     );
   }
 
-  Widget _buildParagraph(String text, Color FontColor) {
+  Widget _buildParagraph(String text, Color fontColor) {
     return Text(
       text,
-      style: TextStyle(fontSize: 16, color: FontColor),
+      style: TextStyle(fontSize: 16, color: fontColor),
       textAlign: TextAlign.justify,
     );
   }
 
-  Widget _buildLink(String text, String url, Color FontColor) {
+  Widget _buildLink(String text, String url, Color fontColor) {
     return InkWell(
       onTap: () async {
         if (await canLaunch(url)) {
@@ -131,51 +131,51 @@ class HelpMenu extends StatelessWidget {
     );
   }
 
-  Widget _buildInstructionalContent(Color FontColor) {
+  Widget _buildInstructionalContent(Color fontColor) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _buildSubSectionTitle('1. Navigating the Home Page', FontColor),
+        _buildSubSectionTitle('1. Navigating the Home Page', fontColor),
         _buildParagraph(
             'The home page is your gateway to all the delicious recipes. Here’s how you can navigate and use it effectively:',
-            FontColor),
+            fontColor),
         _buildStep(
             '1.1',
             'Scroll up and down to view all available recipes. Each recipe card provides a brief overview, including the recipe name and an enticing image.',
-            FontColor),
+            fontColor),
         _buildStep(
             '1.2',
             'Hover over any of the recipes to see additional details such as the recipe name, description, prep time, cook time, cuisine, spice level, course, servings, steps, appliances, and ingredients.',
-            FontColor),
+            fontColor),
         _buildStep(
             '1.3',
             'Tap on any recipe card to view detailed information about the recipe, including step-by-step instructions and a complete list of ingredients.',
-            FontColor),
+            fontColor),
         _buildStep(
             '1.4',
             'Use the search bar at the top of the screen to quickly find recipes by name, ingredients, or cuisine type.',
-            FontColor),
+            fontColor),
         _buildStep(
             '1.5',
             'Filter recipes based on dietary preferences, cook time, and difficulty level using the filter options available on the home page.',
-            FontColor),
+            fontColor),
       ],
     );
   }
 
-  Widget _buildSubSectionTitle(String title, Color FontColor) {
+  Widget _buildSubSectionTitle(String title, Color fontColor) {
     return Padding(
       padding: EdgeInsets.only(top: 10),
       child: Text(
         title,
         style: TextStyle(
-            fontSize: 16, fontWeight: FontWeight.bold, color: FontColor),
+            fontSize: 16, fontWeight: FontWeight.bold, color: fontColor),
       ),
     );
   }
 
   Widget _buildStep(
-      String stepNumber, String stepDescription, Color FontColor) {
+      String stepNumber, String stepDescription, Color fontColor) {
     return Padding(
       padding: EdgeInsets.only(left: 10, top: 5),
       child: Row(
@@ -183,12 +183,12 @@ class HelpMenu extends StatelessWidget {
         children: [
           Text(
             '$stepNumber. ',
-            style: TextStyle(fontSize: 16, color: FontColor),
+            style: TextStyle(fontSize: 16, color: fontColor),
           ),
           Expanded(
             child: Text(
               stepDescription,
-              style: TextStyle(fontSize: 16, color: FontColor),
+              style: TextStyle(fontSize: 16, color: fontColor),
               textAlign: TextAlign.justify,
             ),
           ),
@@ -197,47 +197,47 @@ class HelpMenu extends StatelessWidget {
     );
   }
 
-  Widget _buildFAQs(Color FontColor) {
+  Widget _buildFAQs(Color fontColor) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _buildFAQ(
             'Q: How can I edit my dietary preferences?',
             'A: Go to the "Profile" section and update your dietary preferences. This will help us suggest recipes that match your dietary needs.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: How do I navigate to the Home page?',
             'A: Tap on the "Home" tab in the navbar to return to the Home page at any time.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: How do I add a new recipe?',
             'A: Navigate to the "Add Recipe" section by tapping on the "Add Recipe" tab and fill in the required details to share your recipe with the community.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: How can I manage my shopping list?',
             'A: Go to the "Shopping List" section by tapping on the "Shopping List" tab to view and manage your shopping items.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: How do I keep track of items in my pantry?',
             'A: Use the "Pantry" tab to access the "Pantry List" where you can add, edit, and remove items.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: Where can I manage my kitchen appliances?',
             'A: Tap on the "Appliances" tab to view and manage your kitchen appliances.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: How do I view my saved recipes?',
             'A: Navigate to the "Saved Recipes" section by tapping on the "Saved Recipes" tab to see all your saved recipes.',
-            FontColor),
+            fontColor),
         _buildFAQ(
             'Q: How can I update my profile information?',
             'A: Go to the "Profile" section by tapping on the "Profile" tab to update your profile information.',
-            FontColor),
+            fontColor),
       ],
     );
   }
 
-  Widget _buildFAQ(String question, String answer, Color FontColor) {
+  Widget _buildFAQ(String question, String answer, Color fontColor) {
     return Padding(
       padding: EdgeInsets.only(top: 10),
       child: Column(
@@ -246,12 +246,12 @@ class HelpMenu extends StatelessWidget {
           Text(
             question,
             style: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.bold, color: FontColor),
+                fontSize: 16, fontWeight: FontWeight.bold, color: fontColor),
           ),
           SizedBox(height: 5),
           Text(
             answer,
-            style: TextStyle(fontSize: 16, color: FontColor),
+            style: TextStyle(fontSize: 16, color: fontColor),
             textAlign: TextAlign.justify,
           ),
         ],
