2ade09c5b42701b48f39eb7818e721c616fa6c9e
TessaEngelbrecht <u22633601@tuks.co.za>
2024-06-23 15:48:04 +0200

    Changed default image

--- lib/screens/saved_recipes_screen.dart
+++ lib/screens/saved_recipes_screen.dart
@@ -166,7 +166,7 @@ class _SavedRecipesScreenState extends State<SavedRecipesScreen> {
                               name: recipes[index]['name'] ?? '',
                               description: recipes[index]['description'] ?? '',
                               imagePath:
-                                  recipes[index]['photo'] ?? 'assets/pfp.jpg',
+                                  recipes[index]['photo'] ?? 'assets/emptyPlate.jpg',
                               prepTime: recipes[index]['preptime'] ?? 0,
                               cookTime: recipes[index]['cooktime'] ?? 0,
                               cuisine: recipes[index]['cuisine'] ?? '',
