2cc7ed68761039c4d25d1a7bcd19afd47bb18bec
Janicke Jacobs <u21674605@tuks.co.za>
2024-07-10 22:29:32 +0200

    Adjusted the tabs position

--- lib/screens/inventory_screen.dart
+++ lib/screens/inventory_screen.dart
@@ -3,23 +3,57 @@ import '../widgets/appliances_screen.dart';
 import '../widgets/pantry_screen.dart';
 import '../widgets/shopping_list_screen.dart';
 
-class InventoryScreen extends StatelessWidget {
+class InventoryScreen extends StatefulWidget {
+  @override
+  _InventoryScreenState createState() => _InventoryScreenState();
+}
+
+class _InventoryScreenState extends State<InventoryScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Inventory'),
-          bottom: TabBar(
+    final theme = Theme.of(context);
+    final bool isLightTheme = theme.brightness == Brightness.light;
+    final Color textColor = isLightTheme ? Color(0xFF20493C) : Colors.white;
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(10.0),
+          child: TabBar(
+            controller: _tabController,
             tabs: [
               Tab(text: 'Shopping List'),
               Tab(text: 'Pantry'),
               Tab(text: 'Appliances'),
             ],
+            labelColor: textColor,
+            unselectedLabelColor: Color(0xFFDC945F),
+            indicatorColor: textColor,
           ),
         ),
-        body: TabBarView(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: TabBarView(
+          controller: _tabController,
           children: [
             ShoppingListScreen(),
             PantryScreen(),
