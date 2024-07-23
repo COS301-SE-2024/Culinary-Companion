6882a088fe4ae6ec6268a888c557055d54356244
TessaEngelbrecht <u22633601@tuks.co.za>
2024-07-23 08:54:08 +0200

    fixed more linting issues

--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
-import 'package:mockito/mockito.dart';
+//import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:flutter_application_1/screens/home_screen.dart';
 import 'package:flutter_application_1/widgets/help_home.dart';
 
 // Generate a MockClient using the Mockito package.
 @GenerateMocks([http.Client])
-import 'home_screen_test.mocks.dart';
+//import 'home_screen_test.mocks.dart';
 
 void main() {
   group('HomeScreen Widget Tests', () {
-    late MockClient mockClient;
+    //late MockClient mockClient;
 
-    setUp(() {
-      mockClient = MockClient();
-    });
+    // setUp(() {
+    //   mockClient = MockClient();
+    // });
 
     // testWidgets('HomeScreen shows GridView after loading', (WidgetTester tester) async {
     //   // Mock successful HTTP response
