c9074efe1d1c4c2e84ce906ceaeac39d7f039ea1
TessaEngelbrecht <u22633601@tuks.co.za>
2024-07-24 12:17:41 +0200

    Fixed Integration test

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load();
+  //await dotenv.load(); //LLM
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
     url: 'https://gsnhwvqprmdticzglwdf.supabase.co',
