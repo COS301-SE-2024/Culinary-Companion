445d390977a8b8adddfb55948e3f8033acc21893
Janicke Jacobs <u21674605@tuks.co.za>
2024-07-28 17:35:48 +0200

    Made the from screen a seperate file to reduce lines of code in add_recipe_screen

--- lib/widgets/paste_recipe_screen.dart
+++ lib/widgets/paste_recipe_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PastRecipe extends StatelessWidget {
+class PasteRecipe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
