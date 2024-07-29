525dd7944fe82b7388b9199184edccf815e80483
TessMariPark <teresamaripark@gmail.com>
2024-07-29 14:09:29 +0200

    Added function for paste recipe

--- lib/gemini_service.dart
+++ lib/gemini_service.dart
@@ -27,3 +27,21 @@ Future<String> fetchContentHorse() async {
 
   return response.text ?? 'No response text';
 }
+
+// Future<String> fetchFormattedRecipe(String recipeText) async {
+//   await dotenv.load();
+//   final apiKey = dotenv.env['API_KEY'] ?? '';
+//   if (apiKey.isEmpty) {
+//     return 'No API_KEY environment variable';
+//   }
+//   //print('here1');
+//   final model = GenerativeModel(model: 'gemini-1.5-flash', apiKey: apiKey);
+//   final content = [
+//     Content.text('Format the following recipe for database insertion, including fields: name, description, cookTime, prepTime, servingAmount, cuisine, course, spiceLevel, ingredients (name, quantity, unit), methods, appliances (name). Here is the recipe: $recipeText')
+//   ];
+//   final response = await model.generateContent(content);
+ 
+//   print('$response');
+//   return response.text ?? 'No response text';
+  
+// }
