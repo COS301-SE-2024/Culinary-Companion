82cde20ac6962637fb067fc9b18dca5ff59a437c
Janicke Jacobs <u21674605@tuks.co.za>
2024-07-30 20:12:54 +0200

    Improved styling for desktop tutorial section

---
+++ lib/widgets/mobile_tutorial.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+// import 'package:animate_do/animate_do.dart';
+// import '../screens/tutorial_pages.dart';
+
+class MobileTutorial extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Add your mobile tutorial layout here
+      body: Center(
+        child: Text("Mobile Tutorial"),
+      ),
+    );
+  }
+}
