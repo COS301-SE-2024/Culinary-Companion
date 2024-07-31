c2a6e74641e8d011716ee75a6f9cf41139a262e3
Janicke Jacobs <u21674605@tuks.co.za>
2024-07-31 14:13:51 +0200

    Added a basic search page to the navbar

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController _searchController = TextEditingController();
+  List<String> _searchResults = [];
+
+  void _performSearch(String query) {
+    // Replace this with the actual search logic
+    setState(() {
+      _searchResults = [
+        'Recipe 1',
+        'Recipe 2',
+        'Recipe 3'
+      ].where((recipe) => recipe.toLowerCase().contains(query.toLowerCase())).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Recipes'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search',
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () => _performSearch(_searchController.text),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _searchResults.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_searchResults[index]),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
