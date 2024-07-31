05fc440ceb78733b7749f20a6c39afc50cb3376c
Janicke Jacobs <u21674605@tuks.co.za>
2024-07-31 14:48:13 +0200

    Added filter options to the search page

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -9,22 +11,199 @@ class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _searchController = TextEditingController();
   List<String> _searchResults = [];
 
+  List<String> dietaryOptions = [
+    'Vegan',
+    'Vegetarian',
+    'Gluten-Free',
+    'Lactose-Free',
+    'No Banana',
+    'No Nuts',
+    'High Protein',
+    'High Calorie',
+    'Low Calorie',
+    'Low Carb',
+    'Low Sugar'
+  ]; //Change these so it is fetched from database
+
+  final List<String> _courses = ['Main', 'Breakfast', 'Appetizer', 'Dessert'];
+
+  List<String> cuisineType = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeData();
+  }
+
+  Future<void> _initializeData() async {
+    await _loadCuisines();
+  }
+
+  Future<void> _loadCuisines() async {
+    final url =
+        'https://gsnhwvqprmdticzglwdf.supabase.co/functions/v1/userEndpoint';
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({'action': 'getCuisines'}),
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+        setState(() {
+          // Ensure the UI updates after cuisines are loaded
+          cuisineType = data.map<String>((cuisine) {
+            return cuisine['name'].toString();
+          }).toList();
+        });
+        //print(_cuisines);
+      } else {
+        throw Exception('Failed to load cuisines');
+      }
+    } catch (e) {
+      throw Exception('Error fetching cuisines: $e');
+    }
+  }
+
+  Set<String> selectedDietaryOptions = {};
+  Set<String> selectedCourseTypeOptions = {};
+  Set<String> selectedCuisineType = {};
+
   void _performSearch(String query) {
     // Replace this with the actual search logic
     setState(() {
-      _searchResults = [
-        'Recipe 1',
-        'Recipe 2',
-        'Recipe 3'
-      ].where((recipe) => recipe.toLowerCase().contains(query.toLowerCase())).toList();
+      _searchResults = ['Recipe 1', 'Recipe 2', 'Recipe 3']
+          .where((recipe) => recipe.toLowerCase().contains(query.toLowerCase()))
+          .toList();
     });
+  }
+
+  void _openFilterModal() {
+    final theme = Theme.of(context);
+    final bool isLightTheme = theme.brightness == Brightness.light;
+    final Color textColor = isLightTheme ? Color(0xFF20493C) : Colors.white;
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Padding(
+              padding: EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Filter',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 10),
+                    Text('Dietary Constraints:',
+                        style: TextStyle(fontSize: 16)),
+                    Wrap(
+                      spacing: 8.0,
+                      children: dietaryOptions.map((option) {
+                        return ChoiceChip(
+                          label: Text(option),
+                          selected: selectedDietaryOptions.contains(option),
+                          onSelected: (isSelected) {
+                            setState(() {
+                              isSelected
+                                  ? selectedDietaryOptions.add(option)
+                                  : selectedDietaryOptions.remove(option);
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 10),
+                    Text('Course Type:', style: TextStyle(fontSize: 16)),
+                    Wrap(
+                      spacing: 8.0,
+                      children: _courses.map((option) {
+                        return ChoiceChip(
+                          label: Text(option),
+                          selected: selectedCourseTypeOptions.contains(option),
+                          onSelected: (isSelected) {
+                            setState(() {
+                              isSelected
+                                  ? selectedCourseTypeOptions.add(option)
+                                  : selectedCourseTypeOptions.remove(option);
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 10),
+                    Text('Cuisine Type:', style: TextStyle(fontSize: 16)),
+                    Wrap(
+                      spacing: 8.0,
+                      children: cuisineType.map((option) {
+                        return ChoiceChip(
+                          label: Text(option),
+                          selected: selectedCuisineType.contains(option),
+                          onSelected: (isSelected) {
+                            setState(() {
+                              isSelected
+                                  ? selectedCuisineType.add(option)
+                                  : selectedCuisineType.remove(option);
+                            });
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              selectedDietaryOptions.clear();
+                              selectedCourseTypeOptions.clear();
+                              selectedCuisineType.clear();
+                            });
+                          },
+                          child: Text(
+                            'RESET',
+                            style: TextStyle(color: textColor),
+                          ),
+                        ),
+                        ElevatedButton(
+                          child: Text(
+                            'APPLY',
+                            style: TextStyle(color: textColor),
+                          ),
+                          onPressed: () {
+                            // Perform filtering logic here
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search Recipes'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: _openFilterModal,
+          ),
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
