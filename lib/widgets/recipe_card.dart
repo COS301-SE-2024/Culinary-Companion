6813217d762ef306693eef15bd762d9f352c2842
TessMariPark <teresamaripark@gmail.com>
2024-08-07 09:17:22 +0200

    added course for chat context

--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -1324,6 +1324,7 @@ class _RecipeCardState extends State<RecipeCard> {
                     ingredients: widget.ingredients,
                     steps: widget.steps,
                     userId: userId!,
+                    course:widget.course,
                   ),
                 ),
               ],
