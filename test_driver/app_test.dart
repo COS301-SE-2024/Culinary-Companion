430f995ec7abb37fa8daf310652ca0e8276cdb85
TessaEngelbrecht <u22633601@tuks.co.za>
2024-08-07 09:23:30 +0200

    Revert "tried fixing integraTION tests"
    This reverts commit 58ac964c8a801485fc1ef51e54febfb2c91ae26f.

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,41 +1,41 @@
-// import 'package:flutter_driver/flutter_driver.dart';
-// import 'package:test/test.dart';
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
 
-// void main() {
-//   group('Culinary Companion App', () {
-//     late FlutterDriver driver;
+void main() {
+  group('Culinary Companion App', () {
+    late FlutterDriver driver;
 
-//     setUpAll(() async {
-//       driver = await FlutterDriver.connect();
-//     });
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
 
-//     tearDownAll(() async {
-//       await driver.close();
-//     });
+    tearDownAll(() async {
+      await driver.close();
+    });
 
-//     test('Landing Page', () async {
-//       await driver.waitFor(find.byValueKey('loginButton'));
-//       await driver.waitFor(find.byValueKey('signupButton'));
-//       print("landing pass");
-//     });
+    test('Landing Page', () async {
+      await driver.waitFor(find.byValueKey('loginButton'));
+      await driver.waitFor(find.byValueKey('signupButton'));
+      print("landing pass");
+    });
 
-//     test('Login Page', () async {
-//       await driver.tap(find.byValueKey('loginButton'));
-//       await driver.waitFor(find.byValueKey('email'));
-//       await driver.waitFor(find.byValueKey('password'));
-//       await driver.waitFor(find.text('Login'));
-//       print("login pass");
-//     });
+    test('Login Page', () async {
+      await driver.tap(find.byValueKey('loginButton'));
+      await driver.waitFor(find.byValueKey('email'));
+      await driver.waitFor(find.byValueKey('password'));
+      await driver.waitFor(find.text('Login'));
+      print("login pass");
+    });
 
-//     test('Signup Page', () async {
-//       //await driver.tap(find.text('Don\'t have an account? Sign up'));
-//       await driver.tap(find.byValueKey('signupLink'));
-//       await driver.waitFor(find.byValueKey('emailField'));
-//       await driver.waitFor(find.byValueKey('passwordField'));
-//       await driver.waitFor(find.byValueKey('confirmPasswordField'));
-//       await driver.waitFor(find.byValueKey('signupSubmitButton'));
-//       print("signup pass");
-//     });
+    test('Signup Page', () async {
+      //await driver.tap(find.text('Don\'t have an account? Sign up'));
+      await driver.tap(find.byValueKey('signupLink'));
+      await driver.waitFor(find.byValueKey('emailField'));
+      await driver.waitFor(find.byValueKey('passwordField'));
+      await driver.waitFor(find.byValueKey('confirmPasswordField'));
+      await driver.waitFor(find.byValueKey('signupSubmitButton'));
+      print("signup pass");
+    });
 
-//   });
-// }
+  });
+}
