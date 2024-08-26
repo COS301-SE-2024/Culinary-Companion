77887ff3f48959ef582b60631848fae3cf37eed9
Janicke Jacobs <u21674605@tuks.co.za>
2024-08-26 08:55:27 +0200

    Fixed light mode on mobile view for recipe card and added button for dietary preferences on mobile view

--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -533,6 +533,8 @@ class _RecipeCardState extends State<RecipeCard> {
 
     final clickColor =
         theme.brightness == Brightness.light ? Colors.white : Color(0xFF283330);
+    final textColor =
+        theme.brightness == Brightness.light ? Color(0xFF283330) : Colors.white;
 
     showDialog(
       context: context,
@@ -672,8 +674,8 @@ class _RecipeCardState extends State<RecipeCard> {
                                   0.01, // Adjust height
                             ),
                             TabBar(
-                              labelColor: Colors.white,
-                              indicatorColor: Colors.white,
+                              labelColor: textColor,
+                              indicatorColor: textColor,
                               tabs: [
                                 Tab(text: 'Details'),
                                 Tab(text: 'Instructions'),
@@ -741,7 +743,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                         Text(
                                                           'Prep Time:',
                                                           style: TextStyle(
-                                                            color: Colors.white,
+                                                            color: textColor,
                                                             fontSize: 12,
                                                             fontWeight:
                                                                 FontWeight.bold,
@@ -766,8 +768,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                           child: Text(
                                                             '${widget.prepTime} mins',
                                                             style: TextStyle(
-                                                              color:
-                                                                  Colors.white,
+                                                              color: textColor,
                                                               fontSize: 12,
                                                             ),
                                                           ),
@@ -788,7 +789,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                         thickness: 1.8,
                                                         indent: 20,
                                                         endIndent: 0,
-                                                        color: Colors.white,
+                                                        color: textColor,
                                                       ),
                                                     ),
                                                     SizedBox(
@@ -809,7 +810,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                         Text(
                                                           'Cook Time:',
                                                           style: TextStyle(
-                                                            color: Colors.white,
+                                                            color: textColor,
                                                             fontSize: 12,
                                                             fontWeight:
                                                                 FontWeight.bold,
@@ -834,8 +835,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                           child: Text(
                                                             '${widget.cookTime} mins',
                                                             style: TextStyle(
-                                                              color:
-                                                                  Colors.white,
+                                                              color: textColor,
                                                               fontSize: 12,
                                                             ),
                                                           ),
@@ -859,7 +859,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                 thickness: 1.8,
                                                 indent: 20,
                                                 endIndent: 0,
-                                                color: Colors.white,
+                                                color: textColor,
                                               ),
                                             ),
                                             SizedBox(
@@ -877,7 +877,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                 Text(
                                                   'Total Time:',
                                                   style: TextStyle(
-                                                    color: Colors.white,
+                                                    color: textColor,
                                                     fontSize: 12,
                                                     fontWeight: FontWeight.bold,
                                                   ),
@@ -898,7 +898,7 @@ class _RecipeCardState extends State<RecipeCard> {
                                                   child: Text(
                                                     '${widget.prepTime + widget.cookTime} mins',
                                                     style: TextStyle(
-                                                      color: Colors.white,
+                                                      color: textColor,
                                                       fontSize: 12,
                                                     ),
                                                   ),
@@ -947,6 +947,23 @@ class _RecipeCardState extends State<RecipeCard> {
                                                     .size
                                                     .height *
                                                 0.02), // Adjust height to 2% of screen height
+                                        ElevatedButton(
+                                          onPressed: () {},
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: textColor,
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 40, vertical: 20),
+                                          ),
+                                          child: Text(
+                                              'Adjust recipe to cater to my preferences',
+                                              style:
+                                                  TextStyle(color: clickColor)),
+                                        ),
+                                        SizedBox(
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .height *
+                                                0.02),
                                         Text(
                                           "Ingredients:",
                                           style: TextStyle(
