b4e1439c673380d9d273af3c78b2c5b7b67efb19
TessaEngelbrecht <u22633601@tuks.co.za>
2024-09-19 20:03:09 +0200

    added new classes for web utils

---
+++ lib/widgets/web_utils.dart
@@ -0,0 +1,12 @@
+// lib/web_utils.dart
+
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+class WebUtils {
+  void uploadImage(Function(String) onImageUploaded) {
+    throw UnimplementedError(
+        'uploadImage is not implemented on this platform.');
+  }
+}
+
+WebUtils getWebUtils() => WebUtils();
