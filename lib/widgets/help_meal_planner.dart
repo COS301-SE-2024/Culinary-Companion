93eacf2dfa7d18fddd24f5fe52b050d0c1ac699f
Janicke Jacobs <u21674605@tuks.co.za>
2024-09-30 12:18:37 +0200

    Updated user manual links

--- lib/widgets/help_meal_planner.dart
+++ lib/widgets/help_meal_planner.dart
@@ -70,7 +70,7 @@ class HelpMealPlanner extends StatelessWidget {
                               fontColor),
                           _buildLink(
                               'User Manual',
-                              'https://drive.google.com/file/d/1XGoR5GxF5LXKPfil3cms83fkoHv_C3Ps/view?usp=drive_link',
+                              'https://drive.google.com/file/d/1bk8KVHK-NWHhT3xQHaMw1OlOnKmTv9kr/view?usp=drive_link',
                               fontColor),
                           SizedBox(height: 20),
                           _buildSectionTitle('How it works', fontColor),
